7d907ed83f5101b343c472a5ccc3d29d0f64e8fb
Tom Benbow <tom@hellomoon.net>
2024-10-22 14:07:17 -0500

    Display a loading spinner when an htmx request is made

--- public/js/main.js
+++ public/js/main.js
@@ -1,1 +1,16 @@
-console.log('main.js ran');
+let spinnerTimeout;
+
+// Show the spinner after 1 second if the request is still in progress
+document.body.addEventListener('htmx:beforeRequest', (event) => {
+  spinnerTimeout = setTimeout(() => {
+    document.getElementById('spinner').style.visibility = 'visible';
+    document.getElementById('main').style.visibility = 'hidden';
+  }, 300);
+});
+
+// Hide the spinner and clear the timeout immediately after the request is finished
+document.body.addEventListener('htmx:afterRequest', (event) => {
+  clearTimeout(spinnerTimeout);  // Cancel showing the spinner if request finishes quickly
+  document.getElementById('spinner').style.visibility = 'hidden';
+  document.getElementById('main').style.visibility = 'visible';
+});
