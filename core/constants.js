6ba39d946e5fcf17287cb33528b7252abe02a97e
Andy Schwab <59572086+andyschwab@users.noreply.github.com>
2024-10-28 11:05:31 -0400

    Update constants.js

--- core/constants.js
+++ core/constants.js
@@ -1,3 +1,11 @@
+/**
+ * Relationship Levels
+ * 
+ * REQUIRED relationship levels for personas
+ * Relationships must have a single level from this list
+ * 
+ * @readonly
+ */
 const LEVEL = {
   "POSSESS": 1,   // WILL has physical control
   "DIRECT": 2,    // WILL has immediate authority
@@ -13,6 +21,20 @@ const LEVEL = {
   "ACCESS": 12,     // DO cause to interact with my persona [UNUSED IN V1]  
 }
 
+/**
+ * Relationship Confidence
+ * 
+ * Relationships must have a any confidance value ranging from 0 to 1
+ * The score below are default values
+ * 
+ * MAX-SYSTEM = A system declares the relationship between two personas that it instantiates (highest system-level confidence)
+ * HIGH-PROVEN = The relationship is known to exist, and all details are known (highest human-level confidence)
+ * MEDIUM-ASSERTED = The relationship is known to exist, but not all relationship details are available
+ * LOW-INFERRED = The relationship is assumed to exist from other data, but the relationship is not directly observed
+ * MIN-UNKNOWN = The relationship is speculative
+ * 
+ * @readonly
+ */
 const CONFIDENCE = {
   "MAX-SYSTEM": 1,
   "HIGH-PROVEN": .75,
@@ -21,6 +43,14 @@ const CONFIDENCE = {
   "MIN-UNKNOWN": .01,
 }
 
+/**
+ * Platform Types
+ * 
+ * OPTIONAL platform types for personas
+ * Personas may have platform types not listed here
+ * 
+ * @readonly
+ */
 const PLATFORM = {
   "DIRECTORY": "directory",   // Cartographer app data (Participants, Activities, etc.)
   "BAMBOOHR": "bamboohr",
@@ -30,6 +60,15 @@ const PLATFORM = {
   "SLACK": "slack",
 }
 
+/**
+ * Persona Types
+ * 
+ * OPTIONAL persona types for personas
+ * Personas may have types not listed here
+ * The meaning of each type is platform-specific
+ * 
+ * @readonly
+ */
 const TYPE = {
   "PARTICIPANT": "participant",   // a directory reference to a human being
   "RECORD": "record",             // a platform's reference to a human being; records map indirect control to other personas (the record describes but does not grant agency)
