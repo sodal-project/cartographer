11c2c5109132be8de08ae9a20c9333c0c129d7f5
Andy Schwab <59572086+andyschwab@users.noreply.github.com>
2024-10-28 10:52:14 -0400

    Update config.js

--- core/config.js
+++ core/config.js
@@ -2,9 +2,11 @@ const crypto = require('crypto');
 const { readFromMongo, writeToMongo, deleteFromMongo } = require('./mongo.js');
 
 /**
- * Read Config
+ * Read Persistent Module Configuration Data 
  * 
- * @param {object} data - The data to write to the config file
+ * @param {string} moduleName - The name of the module associated with the configuration data
+ * @param {string} optionalKey - The optional key to read from the configuration data
+ * @returns {object} - The configuration data
  */
 async function readConfig(moduleName, optionalKey) {
   const data = await readFromMongo(moduleName);
@@ -17,9 +19,11 @@ async function readConfig(moduleName, optionalKey) {
 }
 
 /**
- * Write Config
+ * Write Persistent Module Configuration Data
  * 
+ * @param {string} moduleName - The name of the module associated with the configuration data
  * @param {object} data - The data to write to the config file
+ * @returns {object|boolean} - The response from the database, or false if an error occurred
  */
 async function writeConfig(moduleName, data) {
   try {
@@ -27,13 +31,16 @@ async function writeConfig(moduleName, data) {
     return response;
   } catch (err) {
     console.error(`Error in writeConfig: ${err}`);
+    return false;
   }
 }
 
 /**
- * Delete Config
+ * Delete Persistent Module Configuration Data
  * 
+ * @param {string} moduleName - The name of the module associated with the configuration data
  * @param {string} property - The property to delete from the namespace
+ * @returns {object|boolean} - The response from the database, or false if an error occurred
  */
 async function deleteConfig(moduleName, property) {
   try {
@@ -41,6 +48,7 @@ async function deleteConfig(moduleName, property) {
     return response;
   } catch (err) {
     console.error(`Error in deleteConfig: ${err}`);
+    return false;
   }
 }
 
