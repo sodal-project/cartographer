6ce02ce41ed386e78ac195408ee462c8cac50cb0
Tom Benbow <tom@hellomoon.net>
2024-10-30 10:41:50 -0500

    Table component filter ui in progress

--- index.js
+++ index.js
@@ -22,21 +22,32 @@ Handlebars.registerHelper('json', function(context) {
 // Register Partials Manually
 const registerPartials = () => {
   const partialsDir = path.join(__dirname, 'components');
-  
+
   // Clear previously cached partials if necessary
   Handlebars.partials = {};
 
-  fs.readdirSync(partialsDir).forEach(function (filename) {
-    const matches = /^([^.]+).hbs$/.exec(filename);
-    if (matches) {
-      const name = matches[1]; // File name without extension
-      const template = fs.readFileSync(path.join(partialsDir, filename), 'utf8');
-      
-      // Use Handlebars directly to register the partial
-      Handlebars.registerPartial(name, template);
-    }
-  });
+  const readPartials = (dir) => {
+    fs.readdirSync(dir).forEach((file) => {
+      const filePath = path.join(dir, file);
+      const stat = fs.statSync(filePath);
+
+      if (stat.isDirectory()) {
+        // Recursively read subdirectory
+        readPartials(filePath);
+      } else if (file.endsWith('.hbs')) {
+        const name = path.relative(partialsDir, filePath).replace(/\\/g, '/').replace('.hbs', '');
+        const template = fs.readFileSync(filePath, 'utf8');
+
+        // Register the partial with the relative path name
+        Handlebars.registerPartial(name, template);
+      }
+    });
+  };
+
+  // Start reading partials from the root directory
+  readPartials(partialsDir);
 };
+
 
 // Middleware to parse URL-encoded bodies (form data)
 app.use(express.urlencoded({ extended: true }));
