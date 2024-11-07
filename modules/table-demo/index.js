1c8797a1b5eca2a0b8bd2ca96bd591ab551726e8
Andy Schwab <59572086+andyschwab@users.noreply.github.com>
2024-11-07 09:59:36 -0500

    Update index.js

--- modules/table-demo/index.js
+++ modules/table-demo/index.js
@@ -5,15 +5,16 @@ const core = require('../../core/core.js');
  * @returns {string} - Compiled HTML content
  */
 async function redraw(formData) {
-  const rows = [
-    { id: 1, name: 'John Doe', phone: '555-555-5555', street: '123 Main St', city: 'Anytown', state: 'CA', zip: '12345' },
-    { id: 2, name: 'Jane Doe', phone: '555-555-5555', street: '123 Main St', city: 'Anytown', state: 'CA', zip: '12345' },
-    { id: 3, name: 'John Smith', phone: '555-555-5555', street: '123 Main St', city: 'Anytown', state: 'CA', zip: '12345' },
-    { id: 4, name: 'Jane Smith', phone: '555-555-5555', street: '123 Main St', city: 'Anytown', state: 'CA', zip: '12345' },
-  ];
+  const exampleTableConfig = {
+    sortProperty: "upn",
+    sortDirection: "ASC",
+    filterNewProperty: "platform",
+    filterNewTerm: "directory",
+    filterNewCondition: "is",
+  }
 
   const data = {
-    tableData: core.personaTable.fromArray(rows, formData)
+    tableData: await core.personaTable.read(exampleTableConfig),
   }
 
   // Render the index.hbs template
@@ -33,30 +34,16 @@ async function index() {
  * @returns {string} - Compiled HTML content
  */
 async function filter(formData) {
-  const rows = [
-    { id: 1, name: 'John Doe', phone: '555-555-5555', street: '123 Main St', city: 'Anytown', state: 'CA', zip: '12345' },
-    { id: 2, name: 'Jane Doe', phone: '555-555-5555', street: '123 Main St', city: 'Anytown', state: 'CA', zip: '12345' },
-    { id: 3, name: 'John Smith', phone: '555-555-5555', street: '123 Main St', city: 'Anytown', state: 'CA', zip: '12345' },
-    { id: 4, name: 'Jane Smith', phone: '555-555-5555', street: '123 Main St', city: 'Anytown', state: 'CA', zip: '12345' },
-  ];
 
   const data = {
-    tableData: core.personaTable.fromArray(rows, formData),
+    tableData: await core.personaTable.read(formData),
     endpoint: '/mod/table-demo/filter/'
   }
 
   return core.client.render('table.hbs', data);
 }
 
-/**
- * Initialize the module
- */
-async function init() {
-  // nothing to do
-}
-
 module.exports = {
   index,
   filter,
-  init,
 };
