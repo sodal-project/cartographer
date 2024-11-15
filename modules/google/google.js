10d6e9bc02680f5e6100af5d8c1afb73b70fa0b8
Andy Schwab <59572086+andyschwab@users.noreply.github.com>
2024-11-15 11:09:54 -0500

    create user personas

--- modules/google/google.js
+++ modules/google/google.js
@@ -1,5 +1,6 @@
 const core = require('../../core/core.js');
 const { google } = require('googleapis');
+const LEVEL = core.constants.LEVEL;
 
 const sync = async (instance) => {
   console.log('Syncing Google instance:', instance.name);
@@ -27,21 +28,31 @@ const sync = async (instance) => {
   });
   const client = google.admin({ version: 'directory_v1', auth });
 
-
+  // Get raw data for this instance
+  const workspaces = [{
+    id: customerId,
+    name: name,
+  }]
   const users = await loadCached(loadUsers, client, instanceId, customerId);
   const groups = await loadCached(loadGroups, client, instanceId, customerId);
-  // const groupMembers = groups.map(async (group) => {
-  //   return {
-  //     members: await loadCached(loadGroupMembers, client, instanceId, customerId, group.id),
-  //     id: group.id
-  //   }
-  // });
-  // const userTokens = users.map(async (user) => {
-  //   return {
-  //     tokens: await loadCached(loadAuthTokens, client, instanceId, customerId, user.id),
-  //     id: user.id
-  //   }
-  // });
+  const groupMembers = await Promise.all(groups.map(async (group) => {
+    return {
+      members: await loadCached(loadGroupMembers, client, instanceId, customerId, group.id),
+      id: group.id
+    }
+  }));
+  const userTokens = await Promise.all(users.map(async (user) => {
+    return await loadCached(loadAuthTokens, client, instanceId, customerId, user.id)
+  }));
+
+  // Map the data to persona objects
+  let personas = [];
+  personas = personas.concat(mapWorkspacePersonas(workspaces));
+  personas = personas.concat(mapUserPersonas(users, customerId));
+
+  await core.cache.save(`allPersonas-${instanceId}`, personas);
+  // Save the personas to the graph
+  // await core.graph.syncPersonas(personas);
 
 }
 
@@ -49,15 +60,95 @@ const sync = async (instance) => {
 // Map imported API data to persona object arrays
 //
 
-const mapOrganizationPersonas = (orgs) => {
-
+const mapWorkspacePersonas = (workspaces) => {
+  return workspaces.map(workspace => {
+    const newWorkspace = {
+      upn: getWorkspaceUpn(workspace.id),
+      id: workspace.id,
+      platform: 'google',
+      type: 'workspace',
+      friendlyName: workspace.name,
+    }
+    core.check.personaObject(newWorkspace);
+    return newWorkspace;
+  })
 }
 
-const mapUserPersonas = (users) => {
+const mapUserPersonas = (users, customerId) => {
+  if(!users) { return [] }
+  if(!customerId) { throw new Error('customerId is required') }
 
+  const workspaceUpn = getWorkspaceUpn(customerId);
+
+  let personas = [];
+
+  const levelMap = {
+    "superAdmin": LEVEL["ADMIN"],
+    "delegatedAdmin": LEVEL["MANAGE"],
+    "member": LEVEL["ACT_AS"],
+  }
+
+  users.forEach(user => {
+    const firstName = user.name?.givenName
+    const lastName = user.name?.familyName
+    const level = user.isAdmin ? 'superAdmin' : user.isDelegatedAdmin ? 'delegatedAdmin' : 'member';
+
+    const newUser = {
+      upn: `upn:google:account:${user.id}`,
+      id: user.id,
+      platform: 'google',
+      type: 'account',
+      friendlyName: user.name.fullName,
+      firstName: firstName,
+      lastName: lastName,
+      authenticationMin: user.isEnrolledIn2Sv ? 2: 1,
+      lastLogin: user.lastLoginTime,
+      primaryEmail: user.primaryEmail,
+      orgUnitPath: user.orgUnitPath,
+      suspended: user.suspended,
+      archived: user.archived,
+      control: [],
+      obey: [],
+    }
+
+    newUser.obey.push({
+      upn: workspaceUpn,
+      level: LEVEL["REALIZE"],
+      confidence: 1
+    })
+
+    if(!user.suspended && !user.archived) {
+
+      // Add the workspace control relationship
+      newUser.control.push({
+        upn: workspaceUpn,
+        level: levelMap[level],
+        confidence: 1
+      })
+
+      let allAliases = [user.primaryEmail]
+      if(user.aliases) { allAliases = allAliases.concat(user.aliases) }
+      if(user.nonEditableAliases) { allAliases = allAliases.concat(user.nonEditableAliases) }
+
+      // Add email aliases
+      allAliases.forEach(email => {
+        const rel = {
+          upn: `upn:email:account:${email}`,
+          level: LEVEL["ALIAS"],
+          confidence: 1
+        }
+        newUser.control.push(rel);
+        newUser.obey.push(rel);
+      })
+    }
+    core.check.personaObject(newUser);
+    personas.push(newUser);
+  });
+
+  return personas;
 }
 
-const mapGroupPersonas = (groups) => {
+const mapGroupPersonas = (groups, customerId) => {
 
 }
 
@@ -131,6 +222,13 @@ const apiCall = async (client, path, call, request) => {
   } while(pageToken);
 
   return responses;
+}
+
+//
+// Helper Functions
+//
+const getWorkspaceUpn = (customerId) => {
+  return `upn:google:workspace:${customerId}`;
 }
 
 module.exports = {
