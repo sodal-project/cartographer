3250780a97fda2077b2fe1fcf32d30e16358e455
Andy Schwab <59572086+andyschwab@users.noreply.github.com>
2024-12-11 14:10:17 -0500

    Update moduleController.js

--- app/controllers/moduleController.js
+++ app/controllers/moduleController.js
@@ -2,11 +2,11 @@ import core from '../../core/core.js';
 
 const handleModuleFunction = async (req, res) => {
   const { moduleName, command } = req.params;
-  let data = req.body;
+  const data = req.body;
 
   // If it's a GET request, clear `data` as it might not be used.
   if (req.method === 'GET') {
-    data = req.query;
+    data.query = req.query;
   }
   
   // Include file data if it exists (for file upload routes)
