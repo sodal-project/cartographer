1275cc85aa0cf9e14a198bc99ef0a23313ebc3a2
David Snelling <david@soulcraft.com>
2025-05-06 09:45:26 -0700

    Initialize Angular application with core setup
    Added the foundational setup for the Angular app "Atlas," including project configuration, service worker, Firebase integration, and state management. Introduced essential components, services, worker scripts, and styles for the dashboard.

--- src/app/models/service.model.ts
+++ src/app/models/service.model.ts
@@ -1,6 +1,51 @@
+export interface Timestamp {
+  seconds: number
+  nanoseconds: number
+}
+
 export interface Service {
   id: string
   name: string
   version: string
   url: string
+}
+
+export interface ServiceAPI {
+  services: Service[]
+  error?: string
+  isLoading: boolean
+  fetchServices: (registryUrl: string) => void
+}
+
+export interface GraphNode {
+  id: string // Unique identifier for the node
+  createdAt: Timestamp // Timestamp of node creation
+  updatedAt: Timestamp // Timestamp of the last update
+  data: { [key: string]: any } // Flexible data associated with the node
+}
+
+export interface GraphNodeIndex {
+  id: string // Unique identifier for the node
+  text: string // Text content to be indexed for full-text search
+  createdAt: Timestamp // Timestamp of node creation
+  updatedAt: Timestamp // Timestamp of the last update
+}
+
+export interface GraphEdge {
+  id: string // Unique identifier for the edge
+  source: string // ID of the source node
+  target: string // ID of the target node
+  label?: string // Optional label describing the relationship
+  verb: EdgeVerbs
+  createdAt: Timestamp // Timestamp of edge creation
+  updatedAt: Timestamp // Timestamp of the last update
+  data?: { [key: string]: any } // Flexible data associated with the edge
+}
+
+export enum EdgeVerbs {
+  AttributedTo = 'attributedTo',
+  Controls = 'controls',
+  Created = 'created',
+  Earned = 'earned',
+  Owns = 'owns'
 }
